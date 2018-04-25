ea162175e55d191db55122c0a7eb3b649dea9db9
Merschi <stephan.otte@web.de>
2018-04-25 08:17:58 +0200

    comments

---
+++ src/app/commment-list/commment-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wm-commment-list',
+  templateUrl: './commment-list.component.html',
+  styleUrls: ['./commment-list.component.css']
+})
+export class CommmentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/comment.ts
@@ -0,0 +1,9 @@
+export class comment {
+	constructor(
+		public text : string,
+		public chatRefId: string,
+		public memberRefId: string,
+		public creationDT: string,
+	){}
+
+}
